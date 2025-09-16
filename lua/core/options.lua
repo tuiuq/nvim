d4c7066a021817aa212b48e2f6d532f1e0792050
tuiu <13719283454@163.com>
2025-09-17 00:07:02 +0800

    feat: enhance core options configuration - add number and relativenumber options - enable termguicolors - configure clipboard options - update indentation settings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,15 @@
+-- Number
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Colors
 vim.opt.termguicolors = true
+
+-- System Clipboard
 vim.opt.clipboard = "unnamedplus"
 
-
+-- indent
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
