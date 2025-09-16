5a51f3fead81dca16e142dab64a4e7c20db3a41b
tuiu <13719283454@163.com>
2025-09-16 23:53:03 +0800

    configure mappings

--- init.lua
+++ init.lua
@@ -3,19 +3,5 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 
-local map = vim.keymap.set
-
-map("n", "S", "<cmd>w<CR>")
-map("n", "Q", "<cmd>q<CR>")
-map({ "n", "v" }, "J", "5j")
-map({ "n", "v" }, "K", "5k")
-map({ "n", "v" }, "H", "5h")
-map({ "n", "v" }, "L", "5l")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<A-]>", "<cmd>tabnext<CR>")
-map("n", "<A-[>", "<cmd>tabprev<CR>")
 
 require("core")
