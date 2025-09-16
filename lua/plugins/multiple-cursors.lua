e78d91efa670eb11df2ca1d35429e01d59629681
tuiu <13719283454@163.com>
2025-09-17 00:55:38 +0800

    feat: update lazy-lock.json and add vim-multiple-cursors plugin - add vim-multiple-cursors plugin configuration file

---
+++ lua/plugins/multiple-cursors.lua
@@ -0,0 +1,17 @@
+return {
+  "terryma/vim-multiple-cursors",
+  event = "VeryLazy",
+  config = function()
+    vim.g.multi_cursor_use_default_mapping=0
+
+    -- Default mapping
+    vim.g.multi_cursor_start_word_key      = '<C-n>'
+    vim.g.multi_cursor_select_all_word_key = '<A-n>'
+    vim.g.multi_cursor_start_key           = 'g<C-n>'
+    vim.g.multi_cursor_select_all_key      = 'g<A-n>'
+    vim.g.multi_cursor_next_key            = '<C-n>'
+    vim.g.multi_cursor_prev_key            = '<C-p>'
+    vim.g.multi_cursor_skip_key            = '<C-x>'
+    vim.g.multi_cursor_quit_key            = '<Esc>'
+  end
+}
