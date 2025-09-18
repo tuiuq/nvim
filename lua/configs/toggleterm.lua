510d159d34dd93c71c62d4d7427f44632b18342c
tuiu <13719283454@163.com>
2025-09-18 10:49:07 +0800

    feat: add toggleterm configuration

---
+++ lua/configs/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  size = 20 | function (term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  direction = "horizontal"
+}
